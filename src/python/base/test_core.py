a779634e9a6155bdebd19229c7ff07595a3f0bcf
sarodz <samirodrigue@hotmail.com>
2022-02-19 13:22:51 -0500

    format

--- src/python/base/test_core.py
+++ src/python/base/test_core.py
@@ -5,8 +5,9 @@ from base import read_image
 def test_read():
     # Arrange
     img_path = "src/python/base/sample_data/sample_image1.png"
+
     # Act
     img = read_image(img_path)
-    # Assert
 
+    # Assert
     assert isinstance(img, np.ndarray)
